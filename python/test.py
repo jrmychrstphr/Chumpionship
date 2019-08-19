e75f433919e4909e7aa3fd49e29ec231de2efc70
jrmychrstphr <jrmychrstphr@gmail.com>
2019-08-19 13:53:33 +0100

    gw data scraping script edited, gw1 data scraped to temp joson file

--- python/test.py
+++ python/test.py
@@ -1,38 +1,11 @@
-x = ['<td>26</td>', '<td class="MatchesTable__MatchesEntry1-sc-1p0h4g1-8 kvxgCL"><strong>Clarendon Drovers</strong><br/>James Hunter</td>', '<td class="MatchesTable__MatchesScore-sc-1p0h4g1-10 ujSSI">v</td>', '<td class="MatchesTable__MatchesEntry2-sc-1p0h4g1-9 cMsXSV"><strong>Pepe in our step</strong><br/>Lee Guthrie</td>']
+import json
+from pathlib import Path
 
-#find the gameweek
-string_start = "<td>"
-string_start_pos = x[0].find(string_start)
-string_start_pos = string_start_pos + len(string_start)
-print("string_start", string_start_pos)
+f = open("../json/2020_database_temp.json")
+data = json.load(f)
+f.close()
 
-string_end_pos = x[0].find("</td>")
-print("string_end", string_end_pos)
+gw_input = 1
 
-string = x[0][string_start_pos:string_end_pos]
-print(string)
-
-
-#find player name 1
-string_start = "</strong><br/>"
-string_start_pos = x[1].find(string_start)
-string_start_pos = string_start_pos + len(string_start)
-print("string_start", string_start_pos)
-
-string_end_pos = x[1].find("</td>")
-print("string_end", string_end_pos)
-
-string = x[1][string_start_pos:string_end_pos]
-print(string)
-
-#find player name 2
-string_start = "</strong><br/>"
-string_start_pos = x[3].find(string_start)
-string_start_pos = string_start_pos + len(string_start)
-print("string_start", string_start_pos)
-
-string_end_pos = x[3].find("</td>")
-print("string_end", string_end_pos)
-
-string = x[3][string_start_pos:string_end_pos]
-print(string)
+for x in range(1, gw_input+1):
+    print(x)
