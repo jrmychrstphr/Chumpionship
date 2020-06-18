d383f1c8fd2e82325a77555fd7187dfc1e29e4cd
jrmychrstphr <jrmychrstphr@gmail.com>
2020-06-18 10:24:38 +0100

    scraped data

--- _rewrite/_html-files/load_database.js
+++ _rewrite/_html-files/load_database.js
@@ -1,6 +1,8 @@
 var filename = '2020_season_data---20200618_0952.json'
 var database_url = "https://raw.githubusercontent.com/jrmychrstphr/Chumpionship/master/_rewrite/"+filename
 
+console.log(database_url)
+
 var database;
     
 $.getJSON(database_url, function(json) { database = json });
