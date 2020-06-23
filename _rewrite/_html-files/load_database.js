c25b651f2bf4f3fb169c0f647095b94a46d7ce8a
jrmychrstphr <jrmychrstphr@gmail.com>
2020-06-23 09:21:00 +0100

    scraped GW30+

--- _rewrite/_html-files/load_database.js
+++ _rewrite/_html-files/load_database.js
@@ -1,5 +1,5 @@
 //var filename = '2020_season_data---20200618_1022.json'
-var filename = '2020_season_data---20200622_1026.json'
+var filename = '2020_season_data---20200623_0919.json'
 var database_url = "https://raw.githubusercontent.com/jrmychrstphr/Chumpionship/master/_rewrite/"+filename
 
 var database;
