adede63d35ec6186ec34e03aaf57f10470ff5c57
jrmychrstphr <jrmychrstphr@gmail.com>
2020-07-03 08:35:57 +0100

    scraped gw32

--- _rewrite/_html-files/common_functions.js
+++ _rewrite/_html-files/common_functions.js
@@ -15,6 +15,21 @@ function return_comma_formatted_number(num) {
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
+function return_ordinal_suffix_of(num) {
+    var j = num % 10,
+        k = num % 100;
+    if (j == 1 && k != 11) {
+        return num + "st";
+    }
+    if (j == 2 && k != 12) {
+        return num + "nd";
+    }
+    if (j == 3 && k != 13) {
+        return num + "rd";
+    }
+    return num + "th";
+}
+
 function return_manager_code_array(input_database) {
         
     array = [];
