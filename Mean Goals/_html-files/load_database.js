bc5dc51bb4baf274599b9aeb573ff84880a76d76
jrmychrstphr <jrmychrstphr@gmail.com>
2020-07-27 10:57:53 +0100

    scraped gw38

--- Mean Goals/_html-files/load_database.js
+++
@@ -1,6 +0,0 @@
-//var filename = '2020_season_data---20200618_1022.json'
-var database_url = "https://raw.githubusercontent.com/jrmychrstphr/Chumpionship/master/Mean%20Goals/2020_season_data---20200719_0026.json"
-
-var database;
-    
-$.getJSON(database_url, function(json) { database = json });
