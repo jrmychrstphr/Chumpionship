ae3d93312c1e6b4499d3cf2ad1d87665d074289b
jrmychrstphr <jrmychrstphr@users.noreply.github.com>
2021-08-03 14:46:56 +0100

    pre season stuff

--- seasons/2022/dynamic_asset_builders/shared/load_database.js
+++ seasons/2022/dynamic_asset_builders/shared/load_database.js
@@ -1,6 +1,5 @@
-//var filename = '2020_season_data---20200618_1022.json'
-var database_url = "https://raw.githubusercontent.com/jrmychrstphr/Chumpionship/master/seasons/2021/database/chumpionship_2021_database.json"
-
+//var database_url = "https://raw.githubusercontent.com/jrmychrstphr/Chumpionship/master/seasons/2021/database/chumpionship_2021_database.json"
+var database_url = "https://raw.githubusercontent.com/jrmychrstphr/Chumpionship/master/seasons/2022/database/2022_database.json"
 var database;
     
 $.getJSON(database_url, function(json) { database = json });
