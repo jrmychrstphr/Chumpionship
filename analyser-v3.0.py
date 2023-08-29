ef839fdf235d60de135d5db2833793c181a04021
jrmychrstphr <jrmychrstphr@users.noreply.github.com>
2023-08-29 11:14:10 +0100

    gw3

--- analyser-v3.0.py
+++ analyser-v3.0.py
@@ -104,35 +104,143 @@ for fixture in d_fixtures:
 from collections import Counter
 
 for gw in range(1,gameweeks_played+1):
-	print(f"Gameweek {gw}")
+	print(f"")
+	print(f"--- Gameweek {gw} ---")
 
+	# Gameweek score
 	def return_combined_gw_score(gw):
 		return sum([x['fixture_score'] for x in d_players if x['gw'] == gw])
 
-	def return_list_of_gameweek_scores(gw):
+	def return_list_of_gw_scores(gw):
 		return [return_combined_gw_score(x) for x in [x for x in range(1,gw+1)]]
 
+
+	def high_low_gw_score_since(gw):
+
+		# Compares current gw combined score to all previous prev_scores
+		# and returns a value based on how long has passed since a higher 
+		# or lower combined score has been posted
+
+		# Positive value: Highest score since
+		# Negative value: Lowest score since
+		# False: No result (eg, highest/lowest of the season)
+
+		prev_scores = list(reversed(return_list_of_gw_scores(gw)))
+		gw_score = return_combined_gw_score(gw)
+		
+		# prev_scores = [12,1,2,3,4,5,6,7,8,9,10,11]
+		# gw_score = prev_scores[0]
+		
+		lower = next((x for x in prev_scores[1:] if x <= gw_score), False)
+		higher = next((x for x in prev_scores[1:] if x >= gw_score), False)
+
+		if lower and lower > 1:
+			res = -(prev_scores[1:].index(lower)+1)
+		elif higher and higher > 1:
+			res = (prev_scores[1:].index(higher)+1)
+		else:
+			res = False
+
+		return res
+
+
 	def return_gw_combined_score_rank(gw):
-		return sorted(return_list_of_gameweek_scores(gw), reverse=True).index(return_combined_gw_score(gw))+1
+		return sorted(return_list_of_gw_scores(gw), reverse=True).index(return_combined_gw_score(gw))+1
 	
 	def return_average_gw_score(gw):
 		return sum([x['fixture_score'] for x in d_players if x['gw'] == gw]) / len([x['fixture_score'] for x in d_players if x['gw'] == gw])
 
+	def gameweek_score_message():
+
+		msg = f"Round {gw}"
+
+		if return_gw_combined_score_rank(gw) == 1:
+			msg += f" was the highest-scoring week of the season so far."
+		elif return_gw_combined_score_rank(gw) <= 5:
+			msg += f" was the {ord(return_gw_combined_score_rank(gw))} highest-scoring week of the season so far."
+		elif return_gw_combined_score_rank(gw) <= len(return_list_of_gw_scores(gw)) and gw > 2:
+			msg += f" was the lowest-scoring week of the season so far."
+		elif high_low_gw_score_since(gw) and high_low_gw_score_since(gw) > 2:
+			msg += f" was the highest-scoring week for {abs(high_low_gw_score_since(gw))} rounds."
+		elif high_low_gw_score_since(gw) and high_low_gw_score_since(gw) < 2:
+			msg += f" was the lowest-scoring week for {abs(high_low_gw_score_since(gw))} rounds."
+
+		# add: was the highest/lowest scoring round for 5+ weeks
+		
+		msg += f" Chumpionship teams scored a combined {comma_format(int(return_combined_gw_score(gw)))}pts"
+		
+		if gw > 1:
+			score_diff = return_combined_gw_score(gw) - return_combined_gw_score(gw-1)
+			if score_diff == 0:
+				msg += f" – the same score as last time out –"
+			if score_diff > 0:
+				msg += f" – {comma_format(int(score_diff))} more than last time out –"
+			if score_diff < 0:
+				msg += f" – {comma_format(int(-score_diff))} fewer than last time out –"
+
+		msg += f" with an average score of {comma_format(return_average_gw_score(gw))}"
+
+		return msg
+		
+	print(gameweek_score_message())
+	
+	# Gameweek transfers
 	def return_combined_gw_transfers_made(gw):
 		return sum([x['transfers_made'] for x in d_players if x['gw'] == gw])
 
+	def return_list_of_gw_transfers_made(gw):
+		return [return_combined_gw_transfers_made(x) for x in [x for x in range(1,gw+1)]]
 
-	print(f"Total combined points scored: {comma_format(return_combined_gw_score(gw))}")
-	print(f"Total combined points ranking: {ord(return_gw_combined_score_rank(gw))}")
-	print(f"Average points scored: {comma_format(return_average_gw_score(gw))}")
-	print(f"Transfers made: {comma_format(return_combined_gw_transfers_made(gw))}")
 
-	gw_chips = [x['chip_played'] for x in d_players if x['gw'] == gw and x['chip_played'].lower() != 'none']
-	print(f"Chips played: {len(gw_chips)}")
-	for key,val in Counter(gw_chips).items():
-		lst = [x['team_name'] for x in d_players if x['chip_played'] == key]
-		print(f"{key}: {val} ({str(', '.join(str(x) for x in lst))})")
-		print()
+	def gameweek_transfers_message():
+		msg = f""
+
+		if gw == 2:
+			msg += f"The first transfer window of the campaign saw"
+			msg += f" {comma_format(int(return_combined_gw_transfers_made(gw)))} changes"
+		elif return_combined_gw_transfers_made(gw) == max(return_list_of_gw_transfers_made(gw)):
+			msg += f"The busiest transfer window of the season so far saw"
+			msg += f" {comma_format(int(return_combined_gw_transfers_made(gw)))} changes"
+		else:
+			msg += f"{comma_format(int(return_combined_gw_transfers_made(gw)))} changes were"
+	
+		msg += f" made to Chumpionship sides ahead of the GW{int(gw)} deadline."
+
+		return msg
+
+	if gw > 1: print(gameweek_transfers_message())	
+
+	# Gameweek chips
+	def return_list_of_gameweek_chips(gw):
+		return [x['chip_played'] for x in d_players if x['gw'] == gw and x['chip_played'].lower() != 'none']
+		# return [x['chip_played'] for x in d_players if x['gw'] == gw and x['chip_played'].lower()]
+
+	def gameweek_chips_message():
+		msg = f""
+
+		gw_chip_count = len(return_list_of_gameweek_chips(gw))
+		gw_chip_dict = Counter(return_list_of_gameweek_chips(gw))
+
+		print(gw_chip_dict)
+
+		if gw_chip_count == 0:
+			return False
+
+		if len(gw_chip_dict) == 1:
+			for key,val in Counter(return_list_of_gameweek_chips(gw)).items():
+				msg += f"{int(val)} {key.title()}"
+				if val > 1: msg += f"s"
+				msg += f" were played in GW{gw}"
+
+		else:
+			msg += f"{int(gw_chip_count)} chips were played in GW{gw}:"
+			for key,val in Counter(return_list_of_gameweek_chips(gw)).items():
+				msg += f" {int(val)} {key.title()}"
+				if val > 1: msg += f"s"
+
+		return msg
+
+	print(gameweek_chips_message())
 
 
 
